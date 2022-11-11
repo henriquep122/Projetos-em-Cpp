58937cff2a3d2a02d08656c636790006bab1a835
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2022-11-11 20:50:00 -0300

    Add exemplo de FOR

---
+++ Programas C++/FOR em C++/FOR-em-C++.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	for (int i = 0; i <= 9; i++){
+		//cout << i << " - ";
+		
+		for(int j = 0; j <= 9; j++){
+		cout << "(" << i << "," << j << ")";	
+			
+		}
+		cout << endl;
+	}	
+		
+	
+
+
+
+
+
+
+	return 0;
+}
