b4f7aae760e1af89ce22da0b80fea77871d11434
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2022-12-15 18:27:05 -0300

    Create 1016 Distância.cpp

---
+++ beecrowd(URI Online)/1016 Distância.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int Dis, M;
+    
+    cin >> Dis;
+    
+    M = Dis*2;
+    
+    cout << M << " minutos" << endl;
+ 
+    return 0;
+}
